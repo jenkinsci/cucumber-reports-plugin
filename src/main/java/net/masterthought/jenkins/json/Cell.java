6c3b61e329938bb892833b63c843b156957d51d8
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2012-04-19 11:14:25 +0100

    Added row and cell support

---
+++ src/main/java/net/masterthought/jenkins/json/Cell.java
@@ -0,0 +1,20 @@
+package net.masterthought.jenkins.json;
+
+public class Cell {
+
+    private String field;
+    private String value;
+
+    public Cell() {                          ;
+
+    }
+    
+    public String getField(){
+        return field;
+    }
+    
+    public String getValue(){
+        return value;
+    }
+
+}
---
+++ src/main/java/net/masterthought/jenkins/json/Row.java
@@ -0,0 +1,16 @@
+package net.masterthought.jenkins.json;
+
+import java.util.List;
+
+public class Row {
+
+    private String[] cells;
+
+    public Row() {
+
+    }
+    
+    public String[] getCells(){
+      return cells;
+    }
+}
