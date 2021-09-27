b230ec0a9e496e1d54343be23e1e21147edeb46b
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2021-09-27 14:58:43 +0100

    SVG

--- src/main/java/net/masterthought/jenkins/CucumberReportBaseAction.java
+++ src/main/java/net/masterthought/jenkins/CucumberReportBaseAction.java
@@ -6,7 +6,7 @@ import net.masterthought.cucumber.ReportBuilder;
 
 public abstract class CucumberReportBaseAction implements Action {
 
-    protected static final String ICON_NAME = "/plugin/cucumber-reports/icon.png";
+    protected static final String ICON_NAME = "/plugin/cucumber-reports/icon.svg";
 
     @Override
     public String getUrlName() {
